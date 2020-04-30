3b8a4c338eda26e9a61d14db1938c32077926d19
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-30 18:41:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do
     name = params[:name]
     breed = params[:breed]
-    age = params[:breed]
+    age = params[:age]
     @puppy = Puppy.new(name: name, breed: breed, age: age)
     erb :display_puppy
   end
